9ab21d5e447856b86387ef5c3ef7a7da6d741a9b
Jim Padilla and Joshua Rinard <jim@>
2016-10-17 11:00:18 -0700

    Define functions and test

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+
+// Addition
+
+var add = function(n1, n2) {
+  return n1 + n2;
+};
+
+var num1 = parseInt(prompt("Enter a number:"));
+var num2 = parseInt(prompt("Enter a second number:"));
+
+var result = add(num1, num2);
+alert(result);
+
+// Subtraction
+
+var subtract = function(n1, n2) {
+  return n1 + n2;
+};
+
+var num1 = parseInt(prompt("Enter a number:"));
+var num2 = parseInt(prompt("Enter a second number:"));
+
+var result = subtract(num1, num2);
+alert(result);
+
+// Multiplication
+
+var multiply = function(n1, n2) {
+  return n1 * n2;
+};
+
+var num1 = parseInt(prompt("Enter a number:"));
+var num2 = parseInt(prompt("Enter a second number:"));
+
+var result = multiply(num1, num2);
+alert(result);
+
+// Division
+
+var divide = function(n1, n2){
+  return n1 / n2;
+};
+
+var num1 = parseInt(prompt("Enter a number"));
+var num2 = parseInt(prompt("Enter a second number"));
+
+var result = divide(num1, num2);
+alert(result);
