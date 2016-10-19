ddcd685376742991b2d34aad93f8f443806d46bf
Jim Padilla and Margie Nevarez <jim@>
2016-10-19 13:49:25 -0700

    Add subtraction, multiplication, division forms and jquery functionality

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@
   // Subtraction
 
   var subtract = function(n1, n2) {
-    return n1 + n2;
+    return n1 - n2;
   };
 
   // Multiplication
@@ -29,10 +29,40 @@ $(document).ready(function(){
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
 
-    var result = (add(number1, number2));
-    $("#output").text(result);
+    var result = add(number1, number2);
+    $("#output-add").text(result);
     event.preventDefault();
   });
+
+  $("form#subtract").submit(function(event) {
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+
+    var result = subtract(number1, number2);
+    $("#output-subtract").text(result);
+    event.preventDefault();
+  });
+
+  $("form#multiply").submit(function(event) {
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+
+    var result = multiply(number1, number2);
+    $("#output-multiply").text(result);
+
+    event.preventDefault();
+  });
+
+  $("form#divide").submit(function(event) {
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+
+    var result = divide(number1, number2);
+    $("#output-divide").text(result);
+
+    event.preventDefault();
+  });
+
 
 
 });
